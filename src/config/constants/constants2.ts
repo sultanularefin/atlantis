d353f1fc74829583001ac527e8669eb13200bd9e
Md. Sultanul Arefin <mhmdarefin@gmail.com>
2024-11-13 16:56:50 +0600

    gcp ke y removed

--- src/config/constants/constants2.ts
+++ src/config/constants/constants2.ts
@@ -1,8 +1,7 @@
-const GOOGLE_MAPS_API_KEY_1 = "AIzaSyB3yn6evnje1uzHO9HFQCsvNt_vusp33Q8";
+const GOOGLE_MAPS_API_KEY_1 = "zHO9HFQCsvNt_vusp33Q8";
 
 export {
     GOOGLE_MAPS_API_KEY_1,
-    // new maps api key= "AIzaSyB3yn6evnje1uzHO9HFQCsvNt_vusp33Q8";
 };
 
 
